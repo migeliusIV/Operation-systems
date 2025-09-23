97566bc5f8045a49b14c92f8096831dde26fdcf7
Migelius_khaligaly <migel.sorokin@yandex.ru>
2025-09-23 14:43:35 +0300

    Lab 2. Ubuntu's passwords update.

--- ubuntu (kali)/lab_2/lab2_ubuntu.sh
+++ ubuntu (kali)/lab_2/lab2_ubuntu.sh
@@ -2,11 +2,11 @@
 
 echo "Gooooooidaaaaa"
 
-names=("stud_51" "stud_52" "stud_53" "stud_54" "stud_55") 
-passwords=("stud51" "stud52" "stud53" "stud54" "stud55")
+names=("stud_51" "stud_52" "stud_53" "stud_54" "stud_55" "admin_kaf") 
+passwords=("stud5100" "stud5200" "stud5300" "stud5400" "stud5500" "adminkaf")
 sudo groupadd group_stud3k
 
-for userID in 0 1 2 3 4; do
+for userID in 0 1 2 3 4 5; do
     sudo useradd ${names[$userID]}
     echo "${names[$userID]}:${passwords[$userID]}" | sudo chpasswd
     sudo usermod -a -G group_stud3k ${names[$userID]}
